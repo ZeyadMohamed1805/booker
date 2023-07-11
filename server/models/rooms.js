f8c727a31dafd291b254ccd70dea11a5ab37a38a
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2023-07-12 02:52:00 +0300

    Completed The Rooms APIs

---
+++ server/models/rooms.js
@@ -0,0 +1,14 @@
+// Modules & Variabels
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+// Room Schema
+const RoomSchema = new Schema({
+    title: { type: String, required: true },
+    price: { type: Number, required: true },
+    maxPeople: { type: Number, required: true },
+    description: { type: String, required: true },
+    roomNumbers: [{ number: Number, unavailableDates: { type: [ Date ] } }]
+}, { timestamps: true });
+
+export default mongoose.model("Room", RoomSchema);
