0614f0a3f84099a7ecd28f2ba757ab3a29c21773
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2023-07-24 18:38:55 +0300

    Updated the Auth Layout

--- client/src/components/layouts/auth/AuthLayout.tsx
+++ client/src/components/layouts/auth/AuthLayout.tsx
@@ -2,16 +2,15 @@ import { ChildrenType } from "@/utils/types";
 
 const AuthLayout = ({ children }: ChildrenType) => {
     return (
-        <div className="w-full h-screen flex justify-center items-center relative overflow-hidden">
-            <div className="min-w-[1550px] w-full shadow-2xl bg-black bg-opacity-90 overflow-hidden relative">
-                <div className="h-full w-full absolute bg-customBlue bg-opacity-20" />
-                <video loop autoPlay className="min-w-[1550px] w-full">
-                    <source src="https://v.ftcdn.net/03/28/01/95/700_F_328019571_GQ4hJnRtqaYM6mIKw02n0WEAw8b282CE_ST.mp4" type="video/mp4" />
-                </video>
+        <div className="w-full min-h-screen flex">
+            <div className="w-full shadow-2xl max-lg:hidden bg-[url('https://as2.ftcdn.net/v2/jpg/04/03/43/37/1000_F_403433712_LaJr5p5r4hLGnXw3FxGOltmAdeAt1LkC.jpg')] bg-no-repeat bg-cover">
+                <div className="w-full h-screen bg-black bg-opacity-40" />
             </div>
-            <div className="min-h-[35rem] w-[350px] max-[500px]:w-4/5 rounded-md flex flex-col justify-between items-center gap-6 p-3 overflow-hidden z-20 bg-customWhite absolute">
-                <img src="/images/main-logo.png" alt="Booker" className="w-[300px] rounded-md -mt-12" />
-                {children}
+            <div className="w-[400px] max-lg:w-full h-screen grid place-items-center bg-customWhite shadow-black shadow-2xl p-5">
+                <div className="min-h-[35rem] max-w-[500px] rounded-md flex flex-col justify-between items-center gap-6 p-3 overflow-hidden">
+                    <img src="/images/main-logo.png" alt="Booker" className="w-[300px] rounded-md -mt-12" />
+                    {children}
+                </div>
             </div>
         </div>
     )
