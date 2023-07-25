034324b82faaab3f10cef9862f938f7df8d224df
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2023-07-25 13:55:06 +0300

    Improved Auth Validation

--- client/src/components/auth/LoginForm.tsx
+++ client/src/components/auth/LoginForm.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 
 const LoginForm = () => {
-    const { handleSubmit, register } = useForm();
+    const { handleSubmit, register, formState: { errors } } = useForm();
     const { push } = useRouter();
 
     const onSubmit = async ({ username, password }: any) => {
@@ -24,8 +24,14 @@ const LoginForm = () => {
     return (
         <form onSubmit={handleSubmit(onSubmit)} className="w-full flex flex-col justify-between gap-16">
             <div className="w-full flex flex-col gap-3">
-                <input {...register("username", { required: "Username is required." })} type="text" placeholder="Username" className="w-full p-3 rounded-md border-solid border-gray-400 border-2 text-gray-400" />
-                <input {...register("password", { required: "Password is required." })} type="password" placeholder="Password" className="w-full p-3 rounded-md border-solid border-gray-400 border-2 text-gray-400" />
+                <input {...register("username", { required: "Username is required.", minLength: { value: 8, message: "Username is less than 8 digits" } })} type="text" placeholder="Username" className="w-full p-3 rounded-md border-solid border-gray-400 border-2 text-gray-400" />
+                <span className="text-red-500">
+                    { errors?.username?.message?.toString() }
+                </span>
+                <input {...register("password", { required: "Password is required.", minLength: { value: 8, message: "Password is less than 8 digits" } })} type="password" placeholder="Password" className="w-full p-3 rounded-md border-solid border-gray-400 border-2 text-gray-400" />
+                <span className="text-red-500">
+                    { errors?.password?.message?.toString() }
+                </span>
                 <button type="submit" className="w-full bg-primary p-3 text-customWhite font-bold hover:bg-opacity-50 cursor-pointer rounded-md duration-200">
                     Submit
                 </button>
